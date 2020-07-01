458041226e170e8d47ce638e6d233f5f9d2b53fd
haiyang9zhang <912837667@qq.com>
2020-07-01 23:03:10 +0800

    first commit

---
+++ deep_sort/track.py
@@ -0,0 +1,168 @@
+# vim: expandtab:ts=4:sw=4
+
+
+class TrackState:
+    Tentative = 1
+    Confirmed = 2
+    Deleted = 3
+    """
+    Enumeration type for the single target track state. Newly created tracks are
+    classified as `tentative` until enough evidence has been collected. Then,
+    the track state is changed to `confirmed`. Tracks that are no longer alive
+    are classified as `deleted` to mark them for removal from the set of active
+    tracks.
+
+    """
+
+
+
+class Track:
+    """
+    A single target track with state space `(x, y, a, h)` and associated
+    velocities, where `(x, y)` is the center of the bounding box, `a` is the
+    aspect ratio and `h` is the height.
+
+    Parameters
+    ----------
+    mean : ndarray
+        Mean vector of the initial state distribution.
+    covariance : ndarray
+        Covariance matrix of the initial state distribution.
+    track_id : int
+        A unique track identifier.
+    n_init : int                                                   # 跟踪被确认前连续检测的次数（状态标识阈值）
+        Number of consecutive detections before the track is confirmed. The
+        track state is set to `Deleted` if a miss occurs within the first
+        `n_init` frames.
+    max_age : int
+        The maximum number of consecutive misses before the track state is
+        set to `Deleted`.
+    feature : Optional[ndarray]
+        Feature vector of the detection this track originates from. If not None,
+        this feature is added to the `features` cache.
+
+    Attributes
+    ----------
+    mean : ndarray
+        Mean vector of the initial state distribution.
+    covariance : ndarray
+        Covariance matrix of the initial state distribution.
+    track_id : int
+        A unique track identifier.       # 唯一的跟踪标识符
+    hits : int
+        Total number of measurement updates.    # 追踪帧数
+    age : int
+        Total number of frames since first occurance.  # 自第一次出现以来的总帧数。
+    time_since_update : int
+        Total number of frames since last measurement update.  # 丢失的帧总数。
+    state : TrackState
+        The current track state.                   # 当前跟踪状态
+    features : List[ndarray]
+        A cache of features. On each measurement update, the associated feature
+        vector is added to this list.           # 特征的缓存。在每次度量更新中，关联的特征向量被添加到这个列表中。
+    class_id : 改跟踪对应的类别
+    """
+
+class Track:
+    def __init__(self, mean, covariance, track_id, n_init, max_age,class_id,
+                 feature=None):
+        self.mean = mean
+        self.covariance = covariance
+        self.track_id = track_id
+        self.hits = 1
+        self.age = 1
+        self.time_since_update = 0
+        self.state = TrackState.Tentative
+
+        self.class_id = class_id
+        self.features = []
+        if feature is not None:
+            self.features.append(feature)
+
+        self._n_init = n_init
+        self._max_age = max_age
+
+    def to_tlwh(self):
+        """Get current position in bounding box format `(top left x, top left y,
+        width, height)`.
+
+        Returns
+        -------
+        ndarray
+            The bounding box.
+
+        """
+        ret = self.mean[:4].copy()
+        ret[2] *= ret[3]
+        ret[:2] -= ret[2:] / 2
+        return ret
+
+    def to_tlbr(self):
+        """Get current position in bounding box format `(min x, miny, max x,
+        max y)`.
+
+        Returns
+        -------
+        ndarray
+            The bounding box.
+
+        """
+        ret = self.to_tlwh()
+        ret[2:] = ret[:2] + ret[2:]
+        return ret
+
+    def predict(self, kf):
+        """Propagate the state distribution to the current time step using a
+        Kalman filter prediction step.
+
+        Parameters
+        ----------
+        kf : kalman_filter.KalmanFilter
+            The Kalman filter.
+
+        """
+        self.mean, self.covariance = kf.predict(self.mean, self.covariance)
+        self.age += 1
+        self.time_since_update += 1
+
+    def update(self, kf, detection):
+        """Perform Kalman filter measurement update step and update the feature
+        cache.
+
+        Parameters
+        ----------
+        kf : kalman_filter.KalmanFilter
+            The Kalman filter.
+        detection : Detection
+            The associated detection.
+
+        """
+        self.mean, self.covariance = kf.update(
+            self.mean, self.covariance, detection.to_xyah())
+        self.features.append(detection.feature)
+
+        self.hits += 1
+        self.time_since_update = 0
+        if self.state == TrackState.Tentative and self.hits >= self._n_init:
+            self.state = TrackState.Confirmed
+
+    def mark_missed(self):
+        """Mark this track as missed (no association at the current time step).
+        """
+        if self.state == TrackState.Tentative:
+            self.state = TrackState.Deleted
+        elif self.time_since_update > self._max_age:
+            self.state = TrackState.Deleted
+
+    def is_tentative(self):
+        """Returns True if this track is tentative (unconfirmed).
+        """
+        return self.state == TrackState.Tentative
+
+    def is_confirmed(self):
+        """Returns True if this track is confirmed."""
+        return self.state == TrackState.Confirmed
+
+    def is_deleted(self):
+        """Returns True if this track is dead and should be deleted."""
+        return self.state == TrackState.Deleted
---
+++ yolo3/30frames_draw.py
@@ -0,0 +1,216 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import division, print_function, absolute_import
+
+from timeit import time
+import warnings
+import cv2
+import numpy as np
+from PIL import Image
+from yolo import YOLO
+import os
+from deep_sort import preprocessing
+from deep_sort import nn_matching
+from deep_sort.detection import Detection
+from deep_sort.tracker1 import Tracker
+from tools import generate_detections as gdet
+import imutils.video
+from videocaptureasync import VideoCaptureAsync
+
+warnings.filterwarnings('ignore')
+
+
+def main(yolo):
+    # Definition of the parameters
+    max_cosine_distance = 0.3
+    nn_budget = None
+    nms_max_overlap = 1.0
+
+    # Deep SORT
+    model_filename = 'model_data/mars-small128.pb'
+    encoder = gdet.create_box_encoder(model_filename, batch_size=1)
+
+    metric = nn_matching.NearestNeighborDistanceMetric("cosine", max_cosine_distance, nn_budget)
+    tracker = Tracker(metric)
+
+    np.random.seed(42)
+    COLORS = np.random.randint(0, 255, size=(200, 3),
+                               dtype="uint8")
+
+    writeVideo_flag = True
+    asyncVideo_flag = False
+
+    file_path = 'highway.mp4'
+
+    # load the COCO class labels our YOLO model was trained on    加载我们的YOLO模型经过培训的COCO类标签
+    labelsPath = os.path.sep.join(["model_data", "coco_classes.txt"])
+    LABELS = open(labelsPath).read().strip().split("\n")
+    # print(str(len(LABELS))+"load successfully")
+    # print(LABELS)
+    class_nums = np.zeros(80)
+    counter = np.zeros(80)
+    track_id_max = -1
+    if asyncVideo_flag:
+        video_capture = VideoCaptureAsync(file_path)
+    else:
+        video_capture = cv2.VideoCapture(file_path)
+
+    if asyncVideo_flag:
+        video_capture.start()
+
+    if writeVideo_flag:
+        if asyncVideo_flag:
+            w = int(video_capture.cap.get(3))
+            h = int(video_capture.cap.get(4))
+        else:
+            w = int(video_capture.get(3))
+            h = int(video_capture.get(4))
+        fourcc = cv2.VideoWriter_fourcc(*'XVID')
+        out = cv2.VideoWriter('highway.avi', fourcc, 30, (w, h))
+        frame_index = -1
+
+    fps = 0.0
+    fps_imutils = imutils.video.FPS().start()
+
+    # frame_cnt = 0
+    while True:
+        ret, frame = video_capture.read()  # frame shape 640*480*3
+        if ret != True:
+            break
+
+        t1 = time.time()
+
+        image = Image.fromarray(frame[..., ::-1])  # bgr to rgb
+        boxs, confidence, class_names = yolo.detect_image(image)
+
+        features = encoder(frame, boxs)
+
+        detections = [Detection(bbox, confidence, feature) for bbox, confidence, feature in
+                      zip(boxs, confidence, features)]
+
+        # Run non-maxima suppression.
+        boxes = np.array([d.tlwh for d in detections])
+        scores = np.array([d.confidence for d in detections])
+        indices = preprocessing.non_max_suppression(boxes, nms_max_overlap, scores)
+        # print("print indices!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+        # print(indices)
+
+        # for i in indices:
+        #    print(str(i)+class_names[i][0])
+
+        # print(indices.shape)
+
+        detections = [detections[i] for i in indices]
+        class_names = [class_names[i] for i in indices]
+        print("class_name:" + str(class_names))
+
+        class_IDs = []
+        current_nums = np.zeros(80)
+        # class_IDs=[]
+        for class_name in class_names:
+            for i, LABEL in enumerate(LABELS):
+                if class_name[0] == LABEL:
+                    current_nums[i] += 1
+                    class_IDs.append(i)
+        # print("person:"+str(current_nums[0]))
+
+        cv2.putText(frame, 'Current', (20, 70), cv2.FONT_HERSHEY_DUPLEX, 0.75, (255, 255, 255), 2)
+        cv2.putText(frame, 'Total', (180, 70), cv2.FONT_HERSHEY_DUPLEX, 0.75, (0, 255, 255), 2)
+
+        x1 = 20
+        y1 = 100
+        for i, cl in enumerate(current_nums):
+            if cl > 0:
+                cv2.putText(frame, LABELS[i] + "=" + str(cl), (x1, y1), cv2.FONT_HERSHEY_DUPLEX, 0.6, (255, 255, 255),
+                            1)  # 当前帧各类别数量
+                y1 = y1 + 20
+
+        for i, det in enumerate(detections):
+            bbox = det.to_tlbr()
+            score = "%.2f" % round(det.confidence * 100, 2)
+            cv2.rectangle(frame, (int(bbox[0]), int(bbox[1])), (int(bbox[2]), int(bbox[3])), (255, 255, 255), 1)
+            cv2.putText(frame, score + '%', (int(bbox[0]), int(bbox[1]) + 10), cv2.FONT_HERSHEY_DUPLEX, 0.3,
+                        (255, 255, 255), 1)
+            # cv2.putText(frame, class_names[i],(int(bbox[0]), int(bbox[1])-5), 0, 5e-3 * 130, (0, 255, 0), 2)
+            # cv2.putText(frame, class_names[i], (int(bbox[0]), int(bbox[1])), 0, 5e-3 * 130,(0, 255, 255), 2)
+
+        print("Total of detections:" + str(len(detections)))
+        # Call the tracker
+        tracker.predict()
+        tracker.update(detections, class_IDs)
+
+        # for i, cl in enumerate(class_nums):
+        #     if cl > 0:
+        #         print("add: " + LABELS[i] + str(cl - class_last_nums[i]))
+
+        for track in tracker.tracks:
+            if not track.is_confirmed() or track.time_since_update > 1:
+                print("not track.is_confirmed() or track.time_since_update > 1: " + str(track.track_id))
+                continue
+            bbox = track.to_tlbr()
+            color = [int(c) for c in COLORS[track.class_id % len(COLORS)]]
+            cv2.rectangle(frame, (int(bbox[0]), int(bbox[1])), (int(bbox[2]), int(bbox[3])), color, 2)
+            cv2.putText(frame, str(track.track_id) + ' ' + LABELS[track.class_id], (int(bbox[0]), int(bbox[1]) - 5),
+                        cv2.FONT_HERSHEY_DUPLEX, 0.4, color, 1)
+
+            if track.track_id > track_id_max:
+                counter[track.class_id] = counter[track.class_id] + 1
+                track_id_max = track.track_id
+            # class_nums[track.class_id].append(track.track_id)
+            # print(str(LABELS[track.class_id])+":"+class_nums[track.class_id])
+            # print("track.id: " + str(track.track_id))
+            # print("track.class_name: " + str(LABELS[track.class_id]))
+
+        print(str(counter))
+        print("--------------------------该帧输出完毕！--------------------------------------")
+
+        # cv2.putText(frame, 'Total', (200, 60), cv2.FONT_HERSHEY_DUPLEX, 0.75, (255, 0, 0), 2)
+        # x2 = 200
+        # y2 = 100
+        # for i, cl in enumerate(class_nums):
+        #     if cl > 0:
+        #         cv2.putText(frame, LABELS[i] + "=" + str(cl), (x2, y2), cv2.FONT_HERSHEY_DUPLEX, 0.5, (255, 0, 0), 2)
+        #         y2 = y2 + 20
+
+        cv2.putText(frame, "FPS: %f" % (fps), (int(20), int(40)), 0, 5e-3 * 200, (0, 255, 0), 3)  # ！！！！！！！！！输出FPS
+        x2 = 180
+        y2 = 100
+        for i, cl in enumerate(counter):
+            if cl > 0:
+                cv2.putText(frame, LABELS[i] + "=" + str(cl), (x2, y2), cv2.FONT_HERSHEY_DUPLEX, 0.6, (0, 255, 255), 1)
+                y2 = y2 + 20
+        # cv2.imshow('', frame)
+
+        if writeVideo_flag:  # and not asyncVideo_flag:
+            # save a frame
+            out.write(frame)
+            frame_index = frame_index + 1
+
+        fps_imutils.update()
+
+        fps = (fps + (1. / (time.time() - t1))) / 2
+
+        # print("FPS = %f"%(fps))
+
+        # frame_cnt += 1
+        # Press Q to stop!
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    fps_imutils.stop()
+    print('imutils FPS: {}'.format(fps_imutils.fps()))
+
+    if asyncVideo_flag:
+        video_capture.stop()
+    else:
+        video_capture.release()
+
+    if writeVideo_flag:
+        out.release()
+
+    cv2.destroyAllWindows()
+
+
+if __name__ == '__main__':
+    main(YOLO())
